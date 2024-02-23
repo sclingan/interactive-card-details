50dc46c8af6d36bb9c76526a0fb1f0baf216ca3e
Sclingan <sclingan1@gmail.com>
2024-02-23 14:02:23 -0600

    Adjusting layout on form

--- src/section.tsx
+++ src/section.tsx
@@ -8,11 +8,13 @@ function Section() {
             <input type='text' name='name' id='name' placeholder="e.g. Jane Appleseed"></input>
             <label htmlFor="number">Card Number</label>
             <input type='text' name='number' id='number' placeholder="e.g. 1234 5678 9123 0000"></input>
+            <div className="exp">
             <label htmlFor="exp-date">Exp. Date (MM/YY)</label>
-            <input type='number' name='exp-date' id='exp-date' placeholder="MM"></input>
-            <input type='number' placeholder="YY"></input>
-            <label htmlFor="cvc">CVC</label>
+            <input type='number' name='exp-date' id='MM' placeholder="MM"></input>
+            <input type='number' id='YY' placeholder="YY"></input>
+            <label htmlFor="cvc" id='cvc-label'>CVC</label>
             <input type='number' name='cvc' id='cvc' placeholder="e.g. 123"></input>
+            </div>
             <button>Confirm</button>
         </form>
     </section>
