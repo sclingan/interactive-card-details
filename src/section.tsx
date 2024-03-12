7e5dab751d1a4187915f065845ecc233c27e62b9
Sclingan <sclingan1@gmail.com>
2024-03-12 14:19:16 -0500

    Added client side form validation

--- src/section.tsx
+++ src/section.tsx
@@ -11,10 +11,10 @@ function Section() {
             <input type='text' name='number' id='number' placeholder="e.g. 1234 5678 9123 0000"></input>
             <div className="exp">
             <label htmlFor="exp-date">Exp. Date (MM/YY)</label>
-            <input type='number' name='exp-date' id='MM' placeholder="MM"></input>
-            <input type='number' id='YY' placeholder="YY"></input>
+            <input type='number' name='exp-date' id='MM' placeholder="MM" min='0' max='12'></input>
+            <input type='number' id='YY' placeholder="YY" min='00' max='99'></input>
             <label htmlFor="cvc" id='cvc-label'>CVC</label>
-            <input type='number' name='cvc' id='cvc' placeholder="e.g. 123"></input>
+            <input type='number' name='cvc' id='cvc' placeholder="e.g. 123" min='000' max='999'></input>
             </div>
             <button onClick={log}>Confirm</button>
         </form>
--- src/validate.tsx
+++ src/validate.tsx
@@ -4,6 +4,13 @@ export default function log(e: Event) {
     const name = document.getElementById('name')!;
     const number = document.getElementById('number')!;
     const numbers = number.value!;
+    const expMo = document.getElementById('MM')!;
+    const expMonth = expMo.value!;
+    const expYY = document.getElementById('YY')!;
+    const expYear = expYY.value!; 
+    const cvcNumber = document.getElementById('cvc')!;
+    const cvc = cvcNumber.value!;
+
     /* check that name is not empty */
     if(name.value === ''){
         console.log('empty');
@@ -19,7 +26,36 @@ export default function log(e: Event) {
         }else{
             if(/^\d+$/.test(numbers)){
                 console.log('true')
+            }else{
+                console.log('failed');
             }
+        }
+    }
+
+    /* check exp date is not empty and numbers only */
+    if(expMonth === ''){
+        console.log('empty exp');
+    }else{
+        if(expMonth.length < 2){
+            console.log('error numbers exp');
+        }
+    }
+
+    /* check that exp date/year is not empty */
+    if(expYear === ''){
+        console.log('empty exp');
+    }else{
+        if(expYear.length < 2){
+            console.log('error numbers exp');
+        }
+    }
+
+    /* check that cvc is not empty or longer than 3 digitd */
+    if(cvc === ''){
+        console.log('empty cvc');
+    }else{
+        if(cvc.length < 3 || cvc.length > 3){
+            console.log('not enough cvc');
         }
     }
 
